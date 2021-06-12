4ca3b6cdea446f78f6e6f9f9a4a07ae9ec2547db
MicheelG1 <micheelgarcia870@gmail.com>
2021-06-11 19:50:19 -0500

    hilos en Csharp

---
+++ HilosEnCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HilosEnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumarThread sumarThread = new SumarThread();
+
+            Thread hilo1 = new Thread(new ParameterizedThreadStart(sumarThread.sumer));
+            Thread hilo2 = new Thread(new ParameterizedThreadStart(sumarThread.sumer));
+            Thread hilo3 = new Thread(new ParameterizedThreadStart(sumarThread.sumer));
+            hilo1.Name = "Hilo 1";
+            hilo2.Name = "Hilo 2";
+            hilo3.Name = "Hilo 3";
+
+            Int32[] parametros1 = { 2, 1 };
+            Int32[] parametros2 = { 2, 2 };
+            Int32[] parametros3 = { 2, 3 };
+            Console.WriteLine("Preparando los hilos");
+            hilo1.Start(parametros1);
+            hilo2.Start(parametros2);
+            hilo3.Start(parametros3);
+            Console.WriteLine("hilos procesando...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HilosEnCSharp/SumarThread.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HilosEnCSharp
+{
+    class SumarThread
+    {
+        public void sumer(Object parametros)
+        {
+            Thread current = Thread.CurrentThread;
+            Int32[] numeros = (Int32[])parametros;
+            Int32 resultado = numeros[0] + numeros[1];
+            Console.WriteLine("Durmiendo por " + resultado + " segundos");
+            Thread.Sleep(resultado * 1000);
+            Console.WriteLine("El resultado de la suma es " + numeros[0] + " + " + numeros[1] + " = " + resultado);
+            Console.WriteLine("hilo " + current.Name + " finalizo su trabajo");
+        }
+    }
+}
